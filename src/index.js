378d8fe741209100bb426ba7e677ff64365b73e3
mdwasifulkabir <wasifulkabir9016@gmail.com>
2025-08-10 13:26:18 +0800

    Extract week data and hour data in arrays

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-async function getWeatherData(location) {
-  const weather_key = "5D3Q7NUNQRC7RC2ES22BG982L"
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${weather_key}&contentType=json`;
+import { getWeatherData } from "./weather";
 
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log(data)
-}
-
-getWeatherData("Selangor")
+const today = getWeatherData("Selangor")
+today.then(info => console.log(info))
---
+++ src/weather.js
@@ -0,0 +1,43 @@
+let week_data = []
+let hours_data = []
+
+async function getWeatherData(location) {
+  const weather_key = "5D3Q7NUNQRC7RC2ES22BG982L"
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${weather_key}&contentType=json`;
+
+  try {
+    const response = await fetch(url)
+    if(!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json()
+    return extractWeatherInfo(data);
+
+  } catch (error) {
+    console.log(`Error: ${error}`)
+    return null
+  }
+}
+
+function extractWeatherInfo(data) {
+  let week = data.days.slice(0, 7)
+  hours_data = data.days[0].hours
+  console.log(hours_data)
+  
+  for (const day of week) {
+    const day_data = {
+      temperature: day.temp,
+      humidity: day.humidity,
+      datetime: day.datetime,
+      description: day.description,
+      conditions: day.conditions,
+      icon: day.icon
+    }
+    
+    week_data.push(day_data)
+    console.log(day_data)
+  }
+}
+
+export {getWeatherData}
